249a2b2335020ded232c5c891c46805a1f0f3e24
Yogesh <myogeshchavan97@gmail.com>
2020-10-13 16:58:14 +0530

    changes for adding complete user infomation in the email sent

--- server/index.js
+++ server/index.js
@@ -15,10 +15,19 @@ app.post('/send', (req, res) => {
       from: req.body.email, // sender address
       to: process.env.email, // list of receivers
       subject: req.body.subject, // Subject line
-      html: req.body.message // plain text body
+      html: `
+      <p>You have a new contact request.</p>
+      <h3>Contact Details</h3>
+      <ul>
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Subject: ${req.body.subject}</li>
+        <li>Message: ${req.body.message}</li>
+      </ul>
+      `
     };
 
-    transporter.sendMail(mailOptions, function(err, info) {
+    transporter.sendMail(mailOptions, function (err, info) {
       if (err) {
         res.status(500).send({
           success: false,
